ddf53f384f2b2f550ef5e21cc8173b38bf7db1c2
Regardo54 <rhymebokhtier@gmail.com>
2024-08-22 01:36:56 +0600

    'Responsive'

--- main/others/routes.py
+++ main/others/routes.py
@@ -8,3 +8,7 @@ def home():
     page_num = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page_num, per_page=5)
     return render_template('home.html', posts=posts, title="Home")
+
+@others.route('/about')
+def about():
+    return render_template('about.html')
